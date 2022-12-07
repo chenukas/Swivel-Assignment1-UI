5f0d8d548fce1ddbe75ae4f0fd0fcbb13849f8cf
Chenuka Sumanasekara <chenuka@swivelgroup.com.au>
2022-12-07 21:42:23 +0530

    Added CI/CD

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,7 +1,7 @@
 import EMPLOYEES_ACTIONS from './actions/employeesActions';
 import INITIAL_STATE from './InitialState';
 
-export const BASE_API_URL = 'http://localhost:5000';
+export const BASE_API_URL = 'https://shrouded-badlands-42265.herokuapp.com';
 
 export {
     EMPLOYEES_ACTIONS,
